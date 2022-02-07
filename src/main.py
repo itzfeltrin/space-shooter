66a6a8d3906e65061341fda9550316d68d05e7a2
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-02-07 16:38:42 -0300

    create enemies and move models

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import pygame
-import random
 import os
 
 # from models.Player import Player
-from lib.constants import BLACK, BLUE, FPS, GREEN, HEIGHT, WIDTH
+from lib.constants import BLACK, BLUE, FPS, GREEN, HEIGHT, RED, WIDTH
+from models.Mob import Mob
+from models.Player import Player
 
 
 # initialize pygame and create window
@@ -12,7 +13,6 @@ pygame.mixer.init()  # for sound
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
-all_sprites = pygame.sprite.Group()
 
 # set up asset folders
 game_folder = os.path.dirname(__file__)
@@ -21,32 +21,14 @@ player_img = pygame.image.load(
     os.path.join(img_folder, 'p1_jump.png')).convert()
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 40))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
-        self.speedx = 0
-
-    def update(self):
-        self.speedx = 0
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.speedx = -8
-        if keystate[pygame.K_RIGHT]:
-            self.speedx = 8
-        self.rect.x += self.speedx
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-
-
+all_sprites = pygame.sprite.Group()
 player = Player()
+mobs = pygame.sprite.Group()
 all_sprites.add(player)
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
 
 # Game Loop
 running = True
--- src/models/Player.py
+++ src/models/Player.py
@@ -1,17 +1,27 @@
 import pygame
 
-from main import player_img
-from lib.constants import HEIGHT, WIDTH
+from lib.constants import GREEN, HEIGHT, WIDTH
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = player_img
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
 
     def update(self):
-        self.rect.x += 5
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
